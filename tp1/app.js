f4bc056f293b21784372a806bb8447458a4f8ff6
Joshua Vandaële <joshua@vandaele.software>
2023-01-27 09:28:42 +0100

    routes

--- tp1/app.js
+++ tp1/app.js
@@ -3,9 +3,42 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
+app.get('/welcome', (req, res) => {
+    res.send('Bienvenue sur le TP 1 du cours d\'architecture logicielle')
+})
+
+app.get('/secret', (req, res) => {
+    res.status(401)
+})
+
+app.get('/error', (req, res) => {
+    err = {
+        "message": ""
+    }
+    res.status(500).json(err)
+})
+
+app.get('/img', (req, res) => {
+    res.sendFile("img.png", { root: __dirname })
+})
+
+app.get('/redirectMe', (req, res) => {
+    res.redirect("https://univ-littoral.fr")
+})
+
+app.get('/users/:name', (req, res) => {
+    res.send('Bienvenue sur la page de ' + req.params["name"])
+})
+
+app.get('/somme', (req, res) => {
+    res.send(req.query.a + req.query.b)
+})
+
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
