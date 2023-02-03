a122170285a8374eebd25d49f9e6927303926ce8
vandaele.joshua <folfyblu@gmail.com>
2023-02-03 16:38:43 +0100

    Liste d'utilisateurs

--- tp2/src/routes/users.js
+++ tp2/src/routes/users.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { createUser, findUser, editUser } = require("../controllers/users");
+const { createUser, findUser, editUser, listUsers } = require("../controllers/users");
 
 router.get("/create", createUser);
 router.get("/find", findUser);
 router.get("/edit", editUser);
+router.get("/list", listUsers);
 
 module.exports = router;
