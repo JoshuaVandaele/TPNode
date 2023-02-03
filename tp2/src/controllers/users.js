78219b1fcad365c0c7f0bb2e22411e596ab695b5
Joshua Vandaële <joshua@vandaele.software>
2023-02-03 10:12:03 +0100

    Fixes issue #2

--- tp2/src/controllers/users.js
+++ tp2/src/controllers/users.js
@@ -1,6 +1,16 @@
 const { insertOne, findOne, updateOne } = require('../services/db/crud')
 
-
+async function userExists(query) {
+  try {
+    const result = await findOne("users", query);
+    console.log(result)
+    console.log(!!result)
+    return !!result
+  } catch (e) {
+    console.log(e);
+    return true
+  } 
+}
 
 async function findUser(req, res, next) {
   try {
@@ -14,8 +24,12 @@ async function findUser(req, res, next) {
 
 async function createUser(req, res, next) {
   try {
-    const result = await insertOne("users", req.query)
-    return res.send(result)
+    if (!await userExists(req.query)) {
+      const result = await insertOne("users", req.query)
+      return res.send(result)
+    } else {
+      return res.send("User already exists")
+    }
   } catch (e) {
     console.log(e)
     return next(e);
@@ -24,13 +38,13 @@ async function createUser(req, res, next) {
 
 async function editUser(req, res, next) {
   try {
-    if (req.query.oldname && req.query.newname) {
+    if (req.query.oldname && req.query.newname && await !userExists(req.query.newname)) {
       console.log({name: req.query.oldname})
       console.log(req.query.newname)
       const result = await updateOne("users", {name: req.query.oldname}, {$set:{name: req.query.newname}})
       return res.send(result)
     } else {
-      res.send("Invalid request")
+      return next()
     }
   } catch (e) {
     console.log(e)
