fb855b78484427222badd5edba8a0bbc711dba04
vandaele.joshua <folfyblu@gmail.com>
2023-02-03 09:03:31 +0100

    Edit user

--- tp2/src/controllers/users.js
+++ tp2/src/controllers/users.js
@@ -1,4 +1,4 @@
-const { insertOne, findOne } = require('../services/db/crud')
+const { insertOne, findOne, updateOne } = require('../services/db/crud')
 
 async function findUser(req, res, next) {
   try {
@@ -18,7 +18,17 @@ async function createUser(req, res, next) {
   }
 }
 
+async function editUser(req, res, next) {
+  try {
+    const result = await updateOne("users", { name: "toto" }, { name: "toto2"})
+    return res.send(result)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
 module.exports = {
   createUser,
-  findUser
+  findUser,
+  editUser
 };
