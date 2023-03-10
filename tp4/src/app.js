d2791c830fecf3601528f6157c56903c50819b1c
vandaele.joshua <folfyblu@gmail.com>
2023-03-10 15:09:11 +0100

    TP4

---
+++ tp4/src/app.js
@@ -0,0 +1,87 @@
+const { REST, Routes, Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+const conf = require('../conf.json');
+const TOKEN = conf.token;
+const fs = require('node:fs');
+const path = require('node:path');
+const commands = [];
+
+// Créer un nouveau client
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+// Grab all the command files from the commands directory you created earlier
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+    if (command.data && command.execute) {
+	    client.commands.set(command.data.name, command);
+        commands.push(command.data.toJSON());
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+}
+
+// Construct and prepare an instance of the REST module
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+// and deploy your commands!
+(async () => {
+	try {
+		console.log(`Started refreshing ${client.commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationGuildCommands(conf.clientId, conf.guildId),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
+
+const eventsPath = path.join(__dirname, 'events');
+
+const eventFiles = fs
+    .readdirSync(eventsPath)
+    .filter((file) => file.endsWith('.js'));
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+
+client.on(Events.InteractionCreate, async (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(
+            `Aucune commande ${interaction.commandName} n'a été trouvée.`
+        );
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({
+            content: 'Erreur dans l\'exécution de la commande !',
+            ephemeral: true,
+        });
+    }
+});
+
+// Le token permet à votre client de se connecter à Discord
+client.login(TOKEN);
