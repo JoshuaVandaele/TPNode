adffdf8f8deba49ea67d652e49c2d971ce5adff7
vandaele.joshua <folfyblu@gmail.com>
2023-03-10 15:16:36 +0100

    Add an optional parameter to /urban

--- tp4/src/commands/urban.js
+++ tp4/src/commands/urban.js
@@ -4,16 +4,27 @@ const { request } = require('undici')
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('urban')
-		.setDescription('Replies with a random urbandictionary definition'),
+		.setDescription('Replies with a random urbandictionary definition, or the one passed as a parameter')
+		.addStringOption(option =>
+			option.setName('word')
+				.setDescription('Word to define')
+		),
 	async execute(interaction) {
-		const definitions = await request("https://api.urbandictionary.com/v0/random")
-		
+		const input = interaction.options.getString("word")
+		let definitions = null;
+
+		if (!input) {
+			definitions = await request("https://api.urbandictionary.com/v0/random")
+		} else {
+			definitions = await request("https://api.urbandictionary.com/v0/define?term=" + input)
+		}
 		const results = await definitions.body.json()
 		const result = results["list"][0]
-		
+
 		const definition = result["definition"]
 		const word = result["word"]
-		
+
+
 		const message = "**" + word + "** - " + definition
 
 		await interaction.reply(message)
