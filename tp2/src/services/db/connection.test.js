9caa5f66e62a2014dd0e688bb9e3161979b6d5d5
Joshua Vandaële <joshua@vandaele.software>
2023-04-07 15:01:36 +0200

    test: :white_check_mark: Added tests for connection.js
    Tests now use a "mock", and are: Connecting to the db, failing to connect to the db, failing to ping the db, getting the collection and failing to get the collection

--- tp2/src/services/db/connection.test.js
+++ tp2/src/services/db/connection.test.js
@@ -1,29 +1,96 @@
-const { connectTodB, getCollection } = require('./connection');
+const MongoClient = require('mongodb').MongoClient;
+let { connectTodB, getCollection } = require('./connection');
 const conf = require('../../../conf.json');
 
-describe('Database connection', () => {
-    test('Connect to the database', async () => {
-        success = true
-        try {
-            client = await connectTodB();
-            client.close();
-        } catch (error) {
-            success = false
-            console.error(error);
-        }
-        expect(success).toBe(true);
-    });
-
+// Mock MongoClient class
+jest.mock('mongodb', () => {
+    const MongoClientMock = {
+        connect: jest.fn(),
+        close: jest.fn(),
+        db: jest.fn(),
+    };
+    return { MongoClient: jest.fn(() => MongoClientMock) };
 });
 
-describe('Collection retrieval', () => {
-    const COLLECTION_NAME = 'users';
+describe('database connection', () => {
+    let client;
 
-    test(`Retrieve the collection '${COLLECTION_NAME}'`, async () => {
-        client = await connectTodB();
+    beforeEach(() => {
+        client = new MongoClient(conf.tp2.databaseUrl);
+    });
 
-        const collection = getCollection(COLLECTION_NAME);
-        expect(collection.collectionName).toBe(COLLECTION_NAME);
-        client.close()
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test('successfully connects to the database', async () => {
+        // Setup
+        client.db.mockReturnValueOnce({
+            command: jest.fn().mockResolvedValueOnce({ ok: 1 }),
+        });
+
+        // Execute
+        const result = await connectTodB();
+
+        // Assert
+        expect(result).toBe(client);
+    });
+
+    test('fails to connect to the database', async () => {
+        // Setup
+        client.connect.mockRejectedValueOnce(new Error('Connection error'));
+
+        // Execute and assert
+        await expect(connectTodB()).rejects.toThrow('Connection error');
+        expect(client.close).toHaveBeenCalled();
+    });
+
+    test('encounters an error during the ping command', async () => {
+        // Setup
+        client.db.mockReturnValueOnce({
+            command: jest.fn().mockRejectedValueOnce(new Error('Ping error')),
+        });
+
+        // Execute and assert
+        await expect(connectTodB()).rejects.toThrow('Ping error');
+        expect(client.close).toHaveBeenCalled();
+    });
+});
+
+describe('Get collection', () => {
+    let client;
+
+    beforeEach(() => {
+        client = new MongoClient(conf.tp2.databaseUrl);
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+
+    const FAKE_COLLECTION_NAME = 'HERES-A MY BROTHER LUIGI NOW TO TELL YOU A WHOLE HEAPIN A SPAGHETTI PILE OF INFORMACHIONE!';
+    test('Gets the specified collection', () => {
+        // Setup
+        const collectionMock = {};
+        client.db.mockReturnValueOnce({
+            collection: jest.fn().mockReturnValueOnce(collectionMock),
+        });
+
+        // Execute
+        const result = getCollection(FAKE_COLLECTION_NAME);
+
+        // Assert
+        expect(result).toEqual({});
+    });
+
+    test('throws an error when called with an invalid collection name', () => {
+        expect(() => getCollection(95)).toThrow(
+            'Invalid collection name'
+        );
+
+        expect(() => getCollection('')).toThrow(
+            'Invalid collection name'
+        );
     });
 });
